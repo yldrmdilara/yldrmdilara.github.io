02b3f412e341dfcf3f5c18025ae8c44cfb2e795d
Dilara <33101258+yldrmdilara@users.noreply.github.com>
2025-07-10 11:55:14 +0300

    Create parallax.js

---
+++ parallax.js
@@ -0,0 +1,7 @@
+function parallaxScroll(){
+  const scrollY = window.scrollY;
+  document.querySelector('.bg').style.transform = 'translateY(${scrollY * 0.2}px)`;
+  document.querySelector('.mid').style.transform = `translateY(${scrollY * 0.5}px)`;
+  document.querySelector('.fg').style.transform = `translateY(${scrollY * 0.8}px)`;
+}
+window.addEventListener('scroll', parallaxScroll);
