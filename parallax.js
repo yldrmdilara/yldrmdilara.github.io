46dc4d17cbca08c12f1de5d7499103c4bb483c7e
Dilara <33101258+yldrmdilara@users.noreply.github.com>
2025-07-10 12:07:43 +0300

    Update parallax.js

--- parallax.js
+++ parallax.js
@@ -1,6 +1,6 @@
 function parallaxScroll(){
   const scrollY = window.scrollY;
-  document.querySelector('.bg').style.transform = 'translateY(${scrollY * 0.2}px)`;
+  document.querySelector('.bg').style.transform = `translateY(${scrollY * 0.2}px)`;
   document.querySelector('.mid').style.transform = `translateY(${scrollY * 0.5}px)`;
   document.querySelector('.fg').style.transform = `translateY(${scrollY * 0.8}px)`;
 }
