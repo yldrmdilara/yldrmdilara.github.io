a47e4d5a86e54ffaf23d8e4ba6a91678e1aef1ee
Dilara <33101258+yldrmdilara@users.noreply.github.com>
2025-07-30 00:19:27 +0300

    Create darkmode.js

---
+++ darkmode.js
@@ -0,0 +1,13 @@
+fetch("darkmode-button.html")
+  .then(res => res.text())
+  .then(html => {
+    document.body.insertAdjacentHTML("afterbegin", html);
+
+    const btn = document.getElementById("darkModeToggle");
+    btn.addEventListener("click", () => {
+      document.body.classList.toggle("dark-mode");
+      btn.textContent = document.body.classList.contains("dark-mode")
+        ? "☀️ Light Mode"
+        : "🌙 Dark Mode";
+    });
+  });
